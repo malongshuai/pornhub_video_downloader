1cd7467d1a9b97b9ba05c30a9206378974d20558
malongshuai <mlongshuai@gmail.com>
2021-07-01 11:14:13 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gem install ./pornhub_video_downloader*.gem
+gem cleanup pornhub_video_downloader
+echo "command 'gethub' is available, use \`gethub --help' to see how to use"
+
